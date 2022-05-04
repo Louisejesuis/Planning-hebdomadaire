9b7b7a81d643345a04dfd8b3fefdd6bec063bdb5
Louisejesuis <louise.roy@hotmail.fr>
2022-05-04 16:48:21 +0200

    register page

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,17 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import "./styles/app.scss";
 
 // start the Stimulus application
-import './bootstrap';
+import "./bootstrap";
+
+// add class to birthday select form
+var birthdayForm = document.getElementById("registration_form_birthday");
+var birthdaySelect = birthdayForm.childNodes;
+
+birthdaySelect.forEach((select) => {
+    if (select.tagName === "SELECT") {
+        select.classList.add("select", "is-normal", "px-6", "is-size-6");
+    }
+});
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,47 +6,92 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('first_name')
-            ->add('last_name')
-            ->add('birthday', DateType::class)
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+            ->add('first_name', TextType::class, [
+                'label' => 'Prénom',
+                'label_attr' => [
+                    'class' => 'is-size-5 '
                 ],
+                'attr' => [
+                    'class' => 'input is-small'
+                ]
+
+            ])
+            ->add('last_name', TextType::class, [
+                'label' => 'Nom de famille',
+                'label_attr' => [
+                    'class' => 'is-size-5 '
+                ],
+                'attr' => [
+                    'class' => 'input is-small'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'invalid_message' => 'Le format n\'est pas le bon',
+                'label_attr' => [
+                    'class' => 'is-size-5 '
+                ],
+                'attr' => [
+                    'placeholder' => 'example@example.fr',
+                    'class' => 'input is-small'
+
+                ]
+            ])
+            ->add('birthday', DateType::class, [
+                'label' => 'Date de naissance',
+                'format' => 'dd-MM-yyyy',
+                'years' => range(date('Y')-100, date('Y')),
+                'label_attr' => [
+                    'class' => 'is-size-5 '
+                ],
+                'attr' => [
+                    'class' => 'birthday_selector'
+                ],
+
             ])
             ->add('plainPassword', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'label_attr' => [
+                    'class' => 'is-size-5 '
+                ],
+                'attr' => [
+                    'class' => 'input is-small'
+                ],
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Le mot de passe doit être au moins de {{ limit }} cara',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
-            ]);
+            ])
+            ->add('save', SubmitType::class, [
+    'attr' => ['class' => 'button mt-2'],
+    'label' => 'Créer un compte'
+
+]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
